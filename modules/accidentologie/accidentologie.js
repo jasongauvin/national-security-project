689907a751b732bdb672e584174efdde20b466f7
samkach <Bakkach.Abdessamad@gmail.com>
2018-08-08 00:58:28 +0100

    LOGO

--- modules/accidentologie/accidentologie.js
+++ modules/accidentologie/accidentologie.js
@@ -374,3 +374,37 @@ function actualiserCoucheAccident() {
 
 }
 // /FONCTION D'ACTUALISATION DE LA COUCHE ACCIDENT
+
+pulse([-6.83435440063476, 34.0317497029135]);
+
+// Pulse at lonlat
+function pulse(lonlat) {
+    var nb = 6;
+    for (var i = 0; i < nb; i++) {
+        setTimeout(function () {
+            pulseFeature(ol.proj.transform(lonlat, 'EPSG:4326', map.getView().getProjection()));
+        }, i * 500);
+    };
+}
+
+// Pulse feature at coord
+function pulseFeature(coord) {
+
+    var f = new ol.Feature(new ol.geom.Point(coord));
+    f.setStyle(new ol.style.Style(
+        {
+            image: new ol.style["Circle"](
+                {
+                    radius: 30,
+                    points: 4,
+                    stroke: new ol.style.Stroke({ color: "red", width: 2 })
+                })
+        }));
+    map.animateFeature(f, new ol.featureAnimation.Zoom(
+        {
+            fade: ol.easing.easeOut,
+            duration: 3000,
+            easing: ol.easing["upAndDown"]
+        }));
+}
+
